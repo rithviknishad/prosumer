8feb7b5ced104dcd727bb11e879f88a516249b4e
Rithvik Nishad <rithvikn2001@gmail.com>
2022-05-23 14:05:31 +0530

    detect missing env var

--- virtual_prosumer/config_loader.py
+++ virtual_prosumer/config_loader.py
@@ -7,12 +7,19 @@ from yaml.loader import SafeLoader
 path_matcher = re.compile(r"\$\{([^}^{]+)\}")
 
 
+class UnconfiguredEnvironmentError(KeyError):
+    pass
+
+
 def path_constructor(_loader, node):
     """Extract the matched value, expand env variable, and replace the match"""
     value = node.value
     match = path_matcher.match(value)
     env_var = match.group()[2:-1]
-    return os.environ.get(env_var) + value[match.end() :]
+    try:
+        return os.environ[env_var] + value[match.end() :]
+    except KeyError as env_no_exist:
+        raise UnconfiguredEnvironmentError(env_var) from env_no_exist
 
 
 yaml.add_implicit_resolver("!path", path_matcher, None, SafeLoader)
