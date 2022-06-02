06731b33d26cca42c5aa4350b879fc7494bf30e7
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-02 15:22:26 +0530

    Adds Base class utility to support kwargs as instance attrs.

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,15 @@
 def acclimate_dict_for_kwargs(source: dict[str, any]) -> dict[str, any]:
     return {
-        k.removeprefix(" "): (
+        k.removeprefix("$"): (
             acclimate_dict_for_kwargs(v) if isinstance(v, dict) else v
         )
-        for k, v in source
+        for k, v in source.items()
     }
+
+
+kwargs_climatize = acclimate_dict_for_kwargs
+
+
+class Base:
+    def __init__(self, **kwargs) -> None:
+        self.__dict__.update(kwargs)
