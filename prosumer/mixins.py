8fd3ee476ccc2a8fcd43fcab15bf655a179723fc
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-04 22:55:55 +0530

    Adds SupportsExport mixin

---
+++ prosumer/mixins.py
@@ -0,0 +1,18 @@
+from functools import cached_property
+
+
+class SupportsExport:
+    def __init__(self, **kwargs) -> None:
+        self.export_allowed = bool(kwargs.pop("can_export", True))
+        self._unit_export_price = (
+            float(kwargs.pop("unit_export_price")) if self.export_allowed else None
+        )
+        super().__init__(**kwargs)
+
+    @cached_property
+    def unit_export_price(self):
+        if not self.export_allowed:
+            raise PermissionError(
+                "Not allowed to read unit_export_price as `export_allowed` is set to False"
+            )
+        return float(self._unit_export_price)
