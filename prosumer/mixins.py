fb759079afdd0509408a754d5cfe3f89c09b3f7a
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-06 07:24:30 +0530

    SupportExport mixin refactors

--- prosumer/mixins.py
+++ prosumer/mixins.py
@@ -5,12 +5,12 @@ class SupportsExport:
     def __init__(self, **kwargs) -> None:
         self.export_allowed = bool(kwargs.pop("can_export", True))
         self._unit_export_price = (
-            float(kwargs.pop("unit_export_price")) if self.export_allowed else None
+            float(kwargs.pop("export_price")) if self.export_allowed else None
         )
         super().__init__(**kwargs)
 
     @cached_property
-    def unit_export_price(self):
+    def export_price(self):
         if not self.export_allowed:
             raise PermissionError(
                 "Not allowed to read unit_export_price as `export_allowed` is set to False"
